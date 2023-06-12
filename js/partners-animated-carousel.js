a9a6a7def984bdb838de2f8a8d346677ccb17dba
InnaShp <107990414+InnaShp@users.noreply.github.com>
2023-06-12 15:19:51 +0300

    Updated animated carousel for partners list

--- js/partners-animated-carousel.js
+++ js/partners-animated-carousel.js
@@ -1,27 +1,27 @@
 const carousel = document.querySelector('.partners__carousel');
 const carouselInner = carousel.querySelector('.partners__carousel-inner');
-const prevButton = carousel.querySelector('.carousel__button--prev');
-const nextButton = carousel.querySelector('.carousel__button--next');
 const partnerCards = carouselInner.querySelectorAll('.partner-card');
 
 let currentIndex = 0;
+let isForward = true;
 
 function showNextSlide() {
   carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
 function updateCarousel(){
-  currentIndex = currentIndex >= partnerCards.length - 1 ? currentIndex === 0 : currentIndex + 1;
-  showNextSlide()
+  if (isForward) {
+    currentIndex++;
+    if (currentIndex === partnerCards.length - 1) {
+      isForward = false;
+    }
+  } else {
+    currentIndex--;
+    if (currentIndex === 0) {
+      isForward = true;
+    }
+  }
+  showNextSlide();
 }
-prevButton.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + partnerCards.length) % partnerCards.length;
-  showNextSlide();
-});
-
-nextButton.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % partnerCards.length;
-  showNextSlide();
-});
 
 setInterval(updateCarousel, 3000);
